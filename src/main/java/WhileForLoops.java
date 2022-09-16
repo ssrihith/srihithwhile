e9e052c3a2319c287e44ed36833cb03b5517cc10
ssrihith <109777088+ssrihith@users.noreply.github.com>
2022-09-16 09:34:54 -0400

    Initial commit

---
+++ src/main/java/WhileForLoops.java
@@ -0,0 +1,47 @@
+
+
+public class WhileForLoops {
+
+        //looping allows for parts of code to repeat
+        //repetition can be a predefined number of times
+        //or a user choice amount
+        //two main kinds of loops while and for
+        //both are similar, but vary in syntax setup
+
+        //while loops require a "counter" be made outside the loop
+        //"counter" checked in condition
+        //"counter" updated inside the loop
+        //leaves when counter check is false
+        //while loops are "nicer" when dealing with String "counters"
+        //  this is a personal preference
+
+
+        //without updating the "counter" we will end up in an
+        //infinite loop as our condition check will never change
+        //this way of looping we leave the amount of times up to the user
+        //we will only stop when they enter the finished word
+        //this is known as a SENTINEL value
+
+
+        //sometimes we want to predetermine how many times we will loop
+        //this can be done with a while loop, but there are most steps
+        //to remember
+        //create counter outside of loop
+        //check counter in loop check
+        //update counter inside
+
+
+        //a for loop is cleaner when we have this predetermined number
+        //of repetitions we want
+        //all those aspects, counter, check, change happen inside the () of the for loop
+        //we do not need to remember to update or create anything because it is
+        //all covered by the syntax of our () sections
+        //for loops have 3 sections in their header
+        //init this is the int i = 0 part as it is initializing our counter, we could initialize it to any value
+        //  this is only run once at the beginning of the loop and never again
+        //condition check is the i < 3 each time through the loop we will make this check
+        //change is the i++ this could be any update to the counter variable
+        //  we want this change to eventually make the condition check false
+        //  this part is executed after all the statements in the body are executed
+
+}
